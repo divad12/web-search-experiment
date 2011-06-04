7aa510d23981148520de61459f93211465289774
david <david@d6d48554-73ac-4f6e-9968-3bd728bdea11>
2011-06-04 16:15:02 +0000

    Mapper now shingles documents and gives the proper key-value output
    git-svn-id: https://smucker.unfuddle.com/svn/smucker_websearch@4 d6d48554-73ac-4f6e-9968-3bd728bdea11

--- dedup/mapper.cpp
+++ dedup/mapper.cpp
@@ -3,6 +3,7 @@
 #include <cstdio>
 #include <cstring>
 #include <iostream>
+#include <tr1/unordered_set>
 
 #include "third_party/rabinhash-64/rabinhash64.h"
 
@@ -14,15 +15,68 @@ const size_t DOCNO_START_LEN = strlen(DOCNO_START_TAG);
 const char* DOC_START_TAG = "<DOC>";
 const char* DOC_END_TAG = "</DOC>";
 
+const char* TEXT_START_TAG = "<TEXT>";
+const char* TEXT_END_TAG = "</TEXT>";
+const size_t TEXT_START_LEN = strlen(TEXT_START_TAG);
+
+const char* WHITESPACE = " \t\n";
+const char* DOC_ID_SEP = "|";
+
+// TODO: Should not declare static variables of class type.
+// FIXME: This 3rd-party rabin hash function does not actually generate 64-bit
+//     hashes (just 32-bit).
+RabinHashFunction64 rabinHash(1);
+
+const int SHINGLE_SIZE = 64;
+
 ////////////////////////////////////////////////////////////////////////////////
+// TODO: Use trim from Boost. This is pretty efficient tho.
+void trim(std::string& str) {
+    str.erase(str.find_last_not_of(' ') + 1);
+    str.erase(0, str.find_first_not_of(' '));
+}
+
+std::string numToStr(long long num) {
+    char str[20];
+    sprintf(str, "%lld", num);
+    return std::string(str);
+}
+
 // Work in progress. This just emits the pair <docId,1> for now.
 void emitKeyValuePairs(const std::string& doc) {
+    // TODO: need to worry about unicode?
+
+    std::tr1::unordered_set<long long> shingleSet;
+    size_t textStart = doc.find(TEXT_START_TAG);
+    size_t textEnd = doc.find(TEXT_END_TAG, textStart + TEXT_START_LEN);
+
+    // Get the set of all shingles in this document
+    for (size_t i = textStart; i < textEnd; ) {
+        long long shingle = rabinHash.hash(doc.c_str() + i, SHINGLE_SIZE);
+        shingleSet.insert(shingle);
+
+        // Since there's always a closing </TEXT> on its own line, we don't
+        // have to worry about not finding whitespace / non-whitespace below.
+        i = doc.find_first_of(WHITESPACE, i + 1);
+        i = doc.find_first_not_of(WHITESPACE, i + 1);
+    }
+
+    // Get the document ID string concatenated with the # of unique shingles
+    // TODO: Repetitive code with finding text between tags. Use compile-time
+    //     templates to DRY.
     size_t docnoStart = doc.find(DOCNO_START_TAG);
     size_t docnoEnd = doc.find(DOCNO_END_TAG, docnoStart + DOCNO_START_LEN);
-
-    // TODO: Use boost to trim id string.
     std::string docId = doc.substr(docnoStart + DOCNO_START_LEN, docnoEnd - docnoStart - DOCNO_START_LEN);
-    printf("LongValueSum:%s\t1\n", docId.c_str());
+    trim(docId);
+    docId += DOC_ID_SEP;
+    docId += numToStr(shingleSet.size());
+    const char* docIdCstr = docId.c_str();
+
+    // Now emit the <shingle, docId + size> pairs.
+    for (std::tr1::unordered_set<long long>::iterator it = shingleSet.begin();  // Wow what a mouthful... want C++0x auto
+            it != shingleSet.end(); ++it) {
+        printf("%lld\t%s\n", *it, docIdCstr);
+    }
 }
 
 ////////////////////////////////////////////////////////////////////////////////
@@ -31,7 +85,10 @@ int main() {
 
     std::string doc;
     bool inDoc = false;
-    while (scanf("%[^\n]\n", line) != EOF) {
+
+    // Using scanf instead of getline because it is much more efficient. This
+    // is important since we're streaming in large amount of data.
+    while (scanf("%9999[^\n]\n", line) != EOF) {
         // Assumptions: no nested docs; doc tags are uppercase
         if (inDoc) {
             doc += line;
