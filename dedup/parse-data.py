6fee94fe85b323e7ff810187a1e9e28440709c26
david <david@d6d48554-73ac-4f6e-9968-3bd728bdea11>
2011-06-27 10:52:30 +0000

    Add parse-data.py for parsing JSON and generating report from elastic-mapreduce
    git-svn-id: https://smucker.unfuddle.com/svn/smucker_websearch@34 d6d48554-73ac-4f6e-9968-3bd728bdea11

---
+++ dedup/parse-data.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python
+
+# Read from stdin JSON returned by elastic-mapreduce --describe, and output
+# elapsed times for each step
+
+import sys
+import json
+from datetime import datetime
+
+def readableTimestamp(timestamp):
+  return datetime.fromtimestamp(int(timestamp)).strftime('%Y-%m-%d %H:%M:%S')
+
+data = json.loads(''.join(sys.stdin.readlines()))
+
+for jobflow in data['JobFlows']:
+  print 'JobFlow', jobflow['JobFlowId'], 'named', jobflow['Name']
+  print '- Ready at', readableTimestamp(jobflow['ExecutionStatusDetail']['ReadyDateTime'])
+  instances = jobflow['Instances']
+  print '-', instances['InstanceCount'], 'instances,', instances['MasterInstanceType'], 'master instance,', instances['SlaveInstanceType'], 'slave instances'
+  print '- Steps:'
+
+  for step in jobflow['Steps']:
+    detail = step['ExecutionStatusDetail']
+    elapsed = str(int(detail['EndDateTime']) - int(detail['StartDateTime'])) if (detail['EndDateTime'] and detail['StartDateTime']) else 'Not done'
+    inputLocation = step['StepConfig']['HadoopJarStep']['Args'][1]
+    # FIXME HACK HACK HACK hardcoded
+    if inputLocation.find('output') != -1:
+      inputLocation = ''
+    else:
+      print
+
+    print '  -', inputLocation, 'on step', step['StepConfig']['Name']
+    print '     -', elapsed, 'seconds: started', readableTimestamp(detail['StartDateTime']), ', ended', readableTimestamp(detail['EndDateTime'])
+
+  print
