a9d7881b323dfa0d1df07e60f0bf655515bf6894
mack <mack@d6d48554-73ac-4f6e-9968-3bd728bdea11>
2011-06-27 12:49:28 +0000

    Modified file store to use new sync_write suggested by Mark
    git-svn-id: https://smucker.unfuddle.com/svn/smucker_websearch@41 d6d48554-73ac-4f6e-9968-3bd728bdea11

--- web/store.php
+++ web/store.php
@@ -1,9 +1,79 @@
 <?php
 
-$sid = $_POST['sid'];
-$path = "data/$sid.txt";
-$actions = $_POST['actions'];
-foreach ($actions as $action) {
-  file_put_contents($path, serialize($action)."\n", FILE_APPEND);
+function sync_write($path, $message) {
+  $file = fopen($path, 'a');
+  flock($file, LOCK_EX);
+  fwrite($file, $message);
+  flock($file, LOCK_UN);
+  fclose($file);
 }
-print_r($actions);
+
+if (empty($_COOKIE['sid'])) {
+  $sid = mt_rand();
+  setcookie('sid', $sid);
+} else {
+  $sid = $_COOKIE['sid'];
+}
+
+$actions = $_POST['actions'];
+file_put_contents("data/test.bak", "$actions\n", FILE_APPEND);
+$message = '';
+$path = "data/$sid.txt";
+foreach ($actions as $action) {
+  //file_put_contents("$path.bak", json_encode($action)."\n", FILE_APPEND);
+  $message .= json_encode($action)."\n";
+}
+sync_write($path, $message);
+
+// TODO: get php doctrine orm working
+//require_once('orm/bootstrap.php');
+//
+//$conn = Doctrine_Manager::connection(DSN);
+//$_COOKIE['user'] = 'mduan';
+//if (empty($_COOKIE['user'])) {
+//  return;
+//} else {
+//  $username = $_COOKIE['user'];
+//  $q = Doctrine_Query::create()
+//          ->select("u.*")
+//          ->from("User u")
+//          ->where("u.name = ?", $username);
+//  print "print query: ".$q->getSqlQuery()."\n";
+//  $user_collection = $q->execute();
+//  if ($user_collection->count() == 0) {
+//    $user = new User();
+//    $user['name'] = $username;
+//    $user->save();
+//  } else {
+//    $user = $user_collection->getFirst();
+//  }
+//}
+//if (empty($_COOKIE['sid'])) {
+//  $session = new Session();
+//  $session->link('user', array($user['ID']));
+//  $session->save();
+//  $sid = $session['ID'];
+//  setcookie('sid', $sid);
+//} else {
+//  $sid = $_COOKIE['sid'];
+//}
+//
+//$path = "data/$sid.txt";
+//$actions = $_POST['actions'];
+//foreach ($actions as $action) {
+//  file_put_contents($path, json_encode($action)."\n", FILE_APPEND);
+//}
+////print_r($actions);
+//
+//print_r($actions);
+//
+//foreach ($actions as $action) {
+//  $event = new Event();
+//  $event->name = $action['name'];
+//  $event->seq = $action['seq'];
+//  $event->time = $action['time'];
+//  $event->save();
+//  //$user = new User();
+//  //$user->save();
+//}
+//$conn->close();
